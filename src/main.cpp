1e74f2a77065b320972e07be697de25bfc4858a6
jonhfercher <johnathanfercher22@gmail.com>
2016-05-08 21:04:38 -0300

    license

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the VSS-Viewer project.
+ *
+ * This Source Code Form is subject to the terms of the GNU GENERAL PUBLIC LICENSE,
+ * v. 3.0. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
+ */
+
+
 #include "graphics.h"
 
 int main(int argc, char** argv){
