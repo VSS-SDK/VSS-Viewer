af8f72c5c84346c81cb2d125a08a3380a6b7c25a
Johnathan Fercher <johnathanfercher22@gmail.com>
2017-11-18 21:05:08 -0200

    rollback

---
+++ buil/src/CMakeFiles/vss-viewer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vss-viewer.dir/main.cpp.o"
+  "CMakeFiles/vss-viewer.dir/graphics.cpp.o"
+  "CMakeFiles/vss-viewer.dir/VSS-Interface/interface.cpp.o"
+  "vss-viewer.pdb"
+  "vss-viewer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vss-viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
