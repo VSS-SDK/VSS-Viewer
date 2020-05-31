61e5e385ce4d851ba8f319c96fe484c42b46ea63
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2020-05-31 13:52:29 -0300

    MVP para testes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 
-EXIT_IF_AN_ERROR_OCCURRED() {
-    LAST_CODE=$?;
-    echo "$LAST_CODE";
-    if [[ $LAST_CODE != 0 ]];
-    then
-        exit $LAST_CODE;
-    fi
-}
-
-TEST() {
-    cd build
-    ./tests
-    EXIT_IF_AN_ERROR_OCCURRED;
-    cd ..
-}
-
-if [[ $1 == "test" ]];
-then
-    TEST;
-else
-    echo "Only supports test"
-fi
+# Entrypoint: The Viewer Binary
+./build/vss-viewer | tee log.txt
